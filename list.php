92c259f42706081654e2472a0189143523c8a22a
shoji namiki <nami.namazun@gmail.com>
2020-03-09 11:41:04 +0000

    大体のhtml部分

---
+++ list.php
@@ -0,0 +1,24 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<title>Todo App</title>
+</head>
+<body>
+	<h1>やることリスト</h1>
+	<form action="">
+		<input type="text" placeholder="タスクを入力してください">
+	</form>
+	<ul>
+		<li>
+			<p>task1</p>
+			<button type="submit" value="" onclick="">削除</button>
+		</li>
+	</ul>
+</body>
+</html>
