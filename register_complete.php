37569bf0402a04185ce28bddc3ade7ea187182f0
shoji namiki <nami.namazun@gmail.com>
2020-03-13 13:31:04 +0000

    usersテーブルのunique設定追加

---
+++ register_complete.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+        <meta charset="utf-8">
+        <title>Register Complete</title>
+        <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+        <div id="container">
+                <h1>登録が完了しました</h1>
+                <button type="button" class="log_btn" onclick="location.href='login.php'"> ログインページへ</button>
+        </div>
+</body>
+</html>
